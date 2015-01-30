a98f9c77c88f84a4feb94ece1f3b3508c475a675
毛康力 <maokangli@sunteng.com>
2015-01-30 09:07:06 +0800

    添加GOTO INVOKE1 ARITYEQ指令

--- reborn/vm.c
+++ reborn/vm.c
@@ -16,7 +16,6 @@ struct vm {
 
 sexp
 sr_extend(sexp env, sexp vals) {
-	int i;
 	return sexp_cons(NULL, vals, env);
 }
 
@@ -24,6 +23,10 @@ sr_extend(sexp env, sexp vals) {
 sexp 
 allocate_activation_frame(int size) {
 	return sexp_make_vector(NULL, sexp_make_fixnum(size), SEXP_ZERO);
+}
+
+int allocate_activation_frame_length(sexp vals) {
+	return sexp_vector_length(vals);
 }
 
 sexp
@@ -34,6 +37,14 @@ activation_frame_argument(sexp env, int i) {
 void
 set_activation_frame_argument(sexp sr, int i, sexp val) {
 	sexp_vector_data(sr)[i]=val;
+}
+
+sexp 
+sexp_make_closure(int pc, sexp env) {
+  sexp proc = sexp_alloc_type(NULL, pair, SEXP_PROCEDURE);
+  sexp_field(proc, closure, SEXP_PROCEDURE, pc) = pc;
+  sexp_field(proc, closure, SEXP_PROCEDURE, env) = env;
+  return proc;
 }
 
 static int
@@ -117,19 +128,14 @@ FINISH(struct vm *vm) {
 	return 0;
 }
 
-static void
-GOTO(struct vm *vm) {
-	vm->pc++;
-	vm->pc = vm->code[vm->pc];
+static int
+CREATE_CLOSURE(struct vm *vm) {
+	int offset = vm->code[vm->pc + 1];
+	vm->val = sexp_make_closure(vm->pc + offset + 2, vm->env);
+	return 2;
 }
 
 /*
-static void
-CREATE_CLOSURE(struct vm *vm) {
-	int offset = vm->code[vm->pc];
-	vm->val = sexp_make_closure(vm->pc, vm->env);
-}
-
 static void
 FUNCTION_INVOKE(struct vm *vm) {
 	sexp closure = vm->fun;
@@ -141,6 +147,43 @@ FUNCTION_INVOKE(struct vm *vm) {
 	vm->pc = sexp_closure_pc(closure);
 }
 */
+
+static int
+INVOKE1(struct vm *vm) {
+	switch (vm->code[vm->pc]) {
+		case 90:
+			vm->val = sexp_car(vm->val);
+			break;
+		case 91:
+			vm->val = sexp_cdr(vm->val);
+			break;
+		case 92:
+			vm->val = sexp_pairp(vm->val) ? SEXP_TRUE : SEXP_FALSE;
+			break;
+		case 93:
+			vm->val = sexp_symbolp(vm->val) ? SEXP_TRUE:SEXP_FALSE;
+			break;
+		case 94:
+			// TODO
+			// vm->val = sexp_display(NULL, vm->val, NULL);
+			break;
+		case 95:
+			vm->val = sexp_procedurep(vm->val) ? SEXP_TRUE : SEXP_FALSE;
+			break;
+		case 96:
+			vm->val = sexp_nullp(vm->val) ? SEXP_TRUE : SEXP_FALSE;
+			break;
+		case 97:
+			// TODO continuation
+			break;
+		case 98:
+			vm->val = (vm->val == SEXP_EOF) ? SEXP_TRUE : SEXP_FALSE;
+			break;
+		default:
+			return -1;
+	}
+	return 1;
+}
 
 static int
 INVOKE2(struct vm *vm) {
@@ -251,10 +294,22 @@ SET_GLOBAL(struct vm *vm) {
 	global_update(i, vm->val);
 }
 */
-static void
-SHORT_GOTO(struct vm *vm) {
-	int offset = vm->code[vm->pc];
-	vm->pc = vm->pc + offset;
+
+static int
+GOTO(struct vm *vm) {
+	int offset2;
+	int offset1 = vm->code[vm->pc+1];
+	int op = vm->code[vm->pc];
+
+	if (op == 30) {
+		vm->pc += offset1;
+		return 2;
+	} else if (op == 28) {
+		offset2 = vm->code[vm->pc+2];
+		vm->pc += (offset1 + offset2 * 255);
+		return 3;
+	}
+	return -1;
 }
 
 static void
@@ -368,6 +423,23 @@ FUNCTION_GOTO(struct vm *vm) {
 }
 */
 
+static int
+ARITYEQ(struct vm *vm) {
+	char op = vm->code[vm->pc];
+	if (op >= 71 && op < 75) {
+		if (allocate_activation_frame_length(vm->val) != op-70) {
+			return -2;
+		}
+		return 1;
+	} else if (op == 75) {
+		if (allocate_activation_frame_length(vm->val) != vm->code[vm->pc+1]) {
+			return -2;
+		}
+		return 2;
+	} 
+	return -1;
+}
+
 typedef int (*inst_t)(struct vm *vm);
 inst_t instructions[256];
 
@@ -384,8 +456,8 @@ initialize() {
 	instructions[12] = CONSTANT;
 
 	instructions[20] = FINISH;
-	//instructions[28] = LONG_GOTO;
-	//instructions[30] = SHORT_GOTO;
+	instructions[28] = GOTO;
+	instructions[30] = GOTO;
 	// instructions[31] = SHORT_JUMP_FALSE;
 	instructions[32] = EXTEND_ENV;
 	// instructions[33] = UNLINK_ENV;
@@ -395,7 +467,7 @@ initialize() {
 	// instructions[37] = PRESERVE_ENV;
 	// instructions[38] = RESTORE_ENV;
 	// instructions[39] = POP_FUNCTION;
-	// instructions[40] = CREATE_CLOSURE;
+	instructions[40] = CREATE_CLOSURE;
 	// instructions[43] = RETURN;
 	// instructions[45] = FUNCTION_INVOKE;
 	// instructions[46] = FUNCTION_GOTO;
@@ -413,12 +485,28 @@ initialize() {
 	instructions[63] = POP_FRAME;
 	instructions[64] = POP_FRAME;
 	
+	instructions[71] = ARITYEQ;
+	instructions[72] = ARITYEQ;
+	instructions[73] = ARITYEQ;
+	instructions[74] = ARITYEQ;
+	instructions[75] = ARITYEQ;
+	
 	instructions[79] = CONSTANT;
 	instructions[80] = CONSTANT;
 	instructions[81] = CONSTANT;
 	instructions[82] = CONSTANT;
 	instructions[83] = CONSTANT;
 	instructions[84] = CONSTANT;
+
+	instructions[90] = INVOKE1;
+	instructions[91] = INVOKE1;
+	instructions[92] = INVOKE1;
+	instructions[93] = INVOKE1;
+	instructions[94] = INVOKE1;
+	instructions[95] = INVOKE1;
+	instructions[96] = INVOKE1;
+	instructions[97] = INVOKE1;
+	instructions[98] = INVOKE1;
 
 	instructions[100] = INVOKE2;
 	instructions[101] = INVOKE2;
@@ -438,7 +526,7 @@ initialize() {
 int
 run(struct vm *vm, char *code) {
 	int offset;
-	char op;
+	int op;
 	
 	vm->code = code;
 	for ( ; ;) {
@@ -471,7 +559,8 @@ vm_init(struct vm *vm) {
 int
 main(int argc, char *argv[]) {
 	struct vm vm;
-	char bytecode[] = {82, 34, 83, 34, 52, 61, 60, 32, 1, 34, 2, 35, 104, 20};
+	// char bytecode[] = {82, 34, 83, 34, 52, 61, 60, 32, 1, 34, 2, 35, 104, 20};
+	char bytecode[] = {40, 2, 30, 8, 72, 32, 1, 34, 82, 35, 104, 43, 20};
 	int succ;
 	
 	initialize();
