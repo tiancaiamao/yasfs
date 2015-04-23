9087c1a26d463c8fcd5a8b066e67aa736d24a058
毛康力 <maokangli@sunteng.com>
2015-04-23 10:06:59 +0800

    加入了begin  set! 和多参数函数调用的处理

--- scheme2c/cps-convert.scm
+++ scheme2c/cps-convert.scm
@@ -1,48 +1,83 @@
-(define (trivial? x) (or (symbol? x) (number? x) (string? x)))
+(define (trivial? x) (or (number? x) (symbol? x) (string? x) (boolean? x)))
+(define (void) (begin))
 
-;; M变换只处理原子类型和lambda表达式
+;; M变换，处理trivial和lambda表达式
 (define M
   (lambda (exp)
     (match exp
 	   [(? trivial?) exp]
-	   [`(lambda (,var) ,e)
-	    (let ((k$ (gensym 'k$)))
-	      `(lambda (,var ,k$)
+	   [`(lambda (,x ...) ,e)
+	    (let ((k$ (gensym 'k)))
+	      `(lambda (,@x ,k$)
 		 ,(T-c e k$)))])))
 
-;; T-c: sexp x symbol -> sexp
+;; sexp x label => sexp
 (define T-c
   (lambda (exp c)
     (match exp
 	   [(? trivial?) `(,c ,(M exp))]
-	   [`(lambda (,var) ,e) `(,c ,(M exp))]
-	   [`(,f ,e)
-	    (T-k f
+	   [`(begin ,e) (T-c e c)]
+	   [('begin e es ...)
+	    (T-k e (lambda (_)
+		     (T-c `(begin ,@es) c)))]
+	   [`(if ,test ,then ,else)
+	    (let ((k (gensym 'k$)))
+	      `((lambda (,k)
+		  ,(T-k test 
+			(lambda (test$)
+			  `(if ,test$
+			       ,(T-c then k)
+			       ,(T-c else k)))))
+		,c))]
+	   [`(set! ,var ,val)
+	    (T-k val
+		 (lambda (val$)
+		   `(set/k! ,var ,val$
+			    (,c (void)))))]
+	   [(f es ...)
+	    (T-k f 
 		 (lambda (f$)
-		   (T-k e
-			(lambda (e$)
-			  `(,f$ ,e$ ,c)))))])))
+		   (T*-k es
+			 (lambda (es$)
+			   `(,f$ ,@es$ ,c)))))])))
 
-;; T-k: sexp x (sexp -> sexp) -> sexp
+;; sexps x (list => sexp) => sexp
+;; 参数k是接受一个list，返回一个sexp
+(define T*-k
+  (lambda (exps k)
+    (if (null? exps)
+	(k '())
+	(T-k (car exps)
+	     (lambda (first)
+	       (T*-k (cdr exps)
+		     (lambda (remain)
+		       (k (cons first remain)))))))))
+
+;; sexp x (sexp => sexp) => sexp
 (define T-k
   (lambda (exp k)
     (match exp
-	   [(? trivial?) (k (M exp))]
-	   [`(lambda (,var) ,e) (k (M exp))]
-	   [`(,f ,e)
-	    (let* ((rv$ (gensym 'rv$))
-		   (cont `(lambda (,rv$) ,(k rv$))))
-	      (T-k f
-		   (lambda (f$)
-		     (T-k e
-			  (lambda (e$)
-			    `(,f$ ,e$ ,cont))))))])))
-    
+	   [(? trivial?) (k (M exp))]	   
+	   [`(begin ,e)
+	    (T-k e k)]
+	   [('begin e es ...)
+	    (T-k e (lambda (_)
+		     (T-k `(begin ,@es) k)))]
+	   [`(if ,test ,then ,else)
+	    (let* ((rv (gensym 'rv$))
+		   (cont `(lambda (,rv) ,(k rv))))	      
+	      (T-k test
+		   (lambda (test$)
+		     `(if ,test$
+			  ,(T-c then cont)
+			  ,(T-c else cont)))))]
+	   [`(set! ,var ,val)
+	    (T-k val
+		 (lambda (val$)
+		   `(set/k! ,var ,val
+			    ,(k '(void)))))]
+	   [(f es ...)
+	    (let* ((rv (gensym 'rv$))
+		   (cont `(lambda (,rv) ,(k rv))))
+	      (T-c exp cont))])))
 
-
-(T-c '(lambda (x)
-	(lambda (y)
-	  (lambda (z)
-	    (x (y z))))) 'halt)
-
-(T-k '(g a) (lambda (x) x))
