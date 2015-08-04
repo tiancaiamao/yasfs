dbb661c81d854f4149a2206b09ae861bbbe760d7
Arthur <tiancaiamao@gmail.com>
2015-08-04 23:41:28 +0800

    make-thread thread-start! thread-yield! thread-terminate!

--- srfi-18/schedule.scm
+++ srfi-18/schedule.scm
@@ -1,7 +1,7 @@
 (define-record-type :thread
   (make-:thread name state specific cont)
   thread?
-  (name thread-name)
+  (name thread-name :thread-name-set!)
   (state :thread-state :thread-state-set!)
   (specific thread-specific thread-specific-set!)
   (cont :thread-cont :thread-cont-set!))
@@ -18,10 +18,9 @@
       (begin
 	(set! ready-head (cons v '()))
 	(set! ready-tail ready-head))
-      (begin
-	(let ((node (cons v '())))
-	  (set-cdr! ready-tail node)
-	  (set! ready-tail node)))))
+      (let ((node (cons v '())))
+	(set-cdr! ready-tail node)
+	(set! ready-tail node))))
 
 (define (ready-dequeue)
   (let ((ret (car ready-head)))
@@ -29,39 +28,71 @@
     (if (null? ready-head) (set! ready-tail '()))
     ret))
 
-(define this #f)
+(define (ready-delete v)
+  ;; prev not null
+  (define (delete-help prev cur v)
+    (cond
+     ((null? cur) void)
+     ((eq? (car cur) v)
+      (begin 
+	(set-cdr! prev (cdr cur))
+	(if (eq? cur ready-tail)
+	    (set! ready-tail prev))))
+     (else
+      (delete-help cur (cdr cur) v))))
 
+  (cond
+   ((null? ready-head) void)
+   ((eq? (car ready-head) v)
+    (if (eq? ready-head ready-tail)
+	(begin (set! ready-head '())
+	       (set! ready-tail '()))
+	(set! ready-head (cdr ready-head))))
+   (else
+    (delete-help ready-head (cdr ready-head) v))))
+
+(define this #f)
+(define void (begin))
 (define (current-thread)
   this)
 
+;; switch to another thread
+(define (switch-to to)
+  (:thread-state-set! to 'running)
+  (set! this to)
+  ((:thread-cont this) void))
+
+(define (thread-exit-hook)
+  (:thread-state-set! this 'dead)
+  (or (ready-empty?)
+      (let ((next (ready-dequeue)))
+	(switch-to next))))
+
 (define (make-thread thunk . name)
   (set! name (or (symbol? name) (gensym 'thread)))
-  (make-:thread name 'new (begin)
+  (make-:thread name 'new void
 		(lambda (_)
-		  (thunk))))
+		  (thunk)
+		  (thread-exit-hook))))
 
-(define (current/cc)
-  (call/cc (lambda (cc) cc)))
+;; save current thread and switch to another thread
+(define (switch to)
+  (call/cc
+   (lambda (save)
+     (:thread-cont-set! this save)
+     (:thread-state-set! this 'ready)
+     (ready-enqueue this)
+     (switch-to to))))
+
+;; take a appropriate thread and switch to it
+(define (schedule)
+  (let ((next (ready-dequeue)))
+    (switch next)))
 
 (define (thread-yield!)
-  (if (ready-empty?)
-      (schedule)))
+  (if (not (ready-empty?)) (schedule)))
 
-(define (schedule)
-  (let loop ()     
-    (let* ((next (ready-dequeue))
-	   (nc (:thread-cont next)))
-      (:thread-state-set! this 'ready)
-      (ready-enqueue this)
-      (:thread-state-set! next 'running)
-      (let ((cc (current/cc)))
-	(if (procedure? cc)
-	    (begin 
-	      (:thread-cont-set! this cc)
-	      (set! this next)
-	      (nc 'resume))
-	    (loop))))))
-
+;; put thread to ready queue, also trigger a schedule
 (define (thread-start! thread)
   (or (and (thread? thread) 
 	   (eq? (:thread-state thread) 'new))
@@ -71,32 +102,40 @@
   (schedule))
 
 (define (thread-terminate! thread)
-  (if (eq? thread this)
-      (let* ((next (ready-dequeue))
-	     (nc (:thread-cont next)))
-	(set! this next)
-	(nc 'resume))))
+  (:thread-state-set! thread 'dead)
+  (cond
+   ((eq? thread this) ;;suicide
+    (if (ready-empty?) (exit) (thread-exit-hook)))
+   (else
+    (ready-delete thread))))
+    
 
 (set! this (make-thread '()))
 (:thread-state-set! this 'running)
+(:thread-name-set! this 'primordial)
+(call/cc (lambda (cc)
+	   (:thread-cont-set! this cc)))
 
 
-(define counter 10)
-(define (make-thread-thunk name)
-  (letrec ((loop (lambda ()
-		   (and (>= counter 0)
-			(begin
-			  (display "in thread ")
-			  (display name)
-			  (display "; counter = ")
-			  (display counter)
-			  (newline)
-			  (set! counter (- counter 1))
-			  (thread-yield!)
-			  (loop))))))
-    loop))
+(define t (make-thread 
+	   (lambda ()
+	     (printf "print a line ~A~%")
+	     (thread-yield!)
+	     (printf "after yield~%")
+	     (thread-yield!)
+	     (printf "after after yield ~%")
+	     (printf "finish"))))
 
-(begin (thread-start! (make-thread (make-thread-thunk 'a)))
-       (thread-start! (make-thread (make-thread-thunk 'b)))
-       (thread-start! (make-thread (make-thread-thunk 'c))))
+
+(:thread-name-set! t 'create-thread)
+(:thread-state t)
+(eq? t this)
+((:thread-cont this) 'resume)
+
+(thread-start! t)
+(thread-name (car ready-head))
+(thread-name t)
+(thread-yield!)
+      
+
 
