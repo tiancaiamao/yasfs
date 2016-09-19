2eed54f851b41d7166fd538f2d30d7255cc5fc9d
Arthur <tiancaiamao@gmail.com>
2016-09-20 02:44:24 +0800

    the true ZAM...not test yet

--- zen/znic.ml
+++ zen/znic.ml
@@ -17,17 +17,13 @@ end
 module Instruct = struct
   type t =
       Const of int
-    | StackAccess of int
-    | EnvAccess of int
+    | Access of int
     | Closure of t list
-    | Grab of int
-    | Return of int
-    | Restart
-    | Endlet
-    | Let
+    | Tailapply
     | Apply
-    | TailApply
-    | Pushretaddr of t list
+    | Pushmark
+    | Grab
+    | Return
     | Stop
 end
 
@@ -52,77 +48,78 @@ let rec ast2lambda env ast = match ast with
 
 let rec compile exp code = match exp with
     Lambda.Int v -> (Instruct.Const v)::code
-  | Lambda.Var n -> (Instruct.EnvAccess n)::code
-  | Lambda.Fun (n,t) -> (Instruct.Closure (compile_tail exp n))::code
+  | Lambda.Var n -> (Instruct.Access n)::code
+  | Lambda.Fun (n,t) -> (Instruct.Closure (compile_tail exp))::code
   | Lambda.App (t,ts) ->
-    (Instruct.Pushretaddr code)::(compile_tail exp 0)
-and compile_tail exp count = match exp with
+    let init = compile t (Instruct.Apply::code) in
+    Instruct.Pushmark::(List.fold_left (fun a b -> compile b a) init ts)
+and compile_tail exp = match exp with
     Lambda.Int v -> [Instruct.Const v]
-  | Lambda.Var n ->
-    let access = if n < count then Instruct.StackAccess n else Instruct.EnvAccess n in
-    [access; Instruct.Return count]
-  | Lambda.Fun (n,t) -> (Instruct.Grab n)::(compile_tail t n)
+  | Lambda.Var n -> [Instruct.Access n; Instruct.Return]
+  | Lambda.Fun (n,t) -> Instruct.Grab::(compile_tail t)
   | Lambda.App (t,ts) ->
-    let fold_func code exp = compile exp code in
-    let rev_ts = List.rev ts in
-    List.fold_left fold_func (compile t [Instruct.TailApply]) rev_ts
+    (t::ts
+     |> List.rev
+     |> List.map (fun x -> compile x [])
+     |> List.flatten) @ [Instruct.Tailapply]
 
 type result =
     Value of int
   | Lambda of (Instruct.t list * result list)
-  | Epsilon
+  | Eplison
 
 let env_get (e : result list) n = List.nth e n
 
 let env_put (e : result list) v = v :: e
 
-let env_close (e : result list) (s: result Stack.t) = e
-
-module Stack = struct
-  type 'a t = {mutable top: int; mutable data: 'a array; cap: int}
-  let create n = {top=0; data=[||]; cap=n}
-  let push x s =
-    if Array.length s.data = 0 then
-      s.data <- Array.make s.cap x
-    else
-      s.data.(s.top) <- x; s.top <- s.top + 1
-  let pop s = let r = s.data.(s.top-1) in s.top <- s.top-1; r
-  let top s = s.data.(s.top-1)
-  let access n s = s.data.(s.top-1-n)
-end
-
-let step (c, e, s) op =
+let step (c, e, s, r) op =
   match op with
-    | Instruct.Const v -> Stack.push (Value v) s; (c, e, s)
-    | Instruct.StackAccess n -> Stack.push (Stack.access n s) s; (c, e, s)
-    | Instruct.EnvAccess n -> Stack.push (env_get e n) s; (c, e, s)
-    | Instruct.Grab n -> if enough_data s n then (c, e, s) else
-      let cls = Lambda (Instruct.Restart::(Instruct.Grab n)::c, env_close e s) in
-      Stack.push cls s;
-    | Instruct.Closure c1 -> Stack.push (Lambda (c1,e)) s; (c, e, s)
-    | Instruct.Apply ->
-      let v = Stack.pop s in
-      let (c1,e1) = match Stack.pop s with
-          Value _ -> failwith "cannot apply non closure"
-        | Lambda x -> x in
-      Stack.push (Lambda (c,e)) s;
-      (c1, (env_put e1 v), s)
-    | Instruct.Return n ->
-      let v = Stack.pop s in
-      let (c1, e1) = match Stack.pop s with
-          Value _ -> failwith "not a saved return address"
-        | Lambda x -> x in
-      Stack.push v s;
-      (c1, e1, s)
-    | _ -> failwith "not implement"
+    Instruct.Const v -> Stack.push (Value v) s; (c, e, s, r)
+  | Instruct.Access n -> Stack.push (env_get e n) s; (c, e, s, r)
+  | Instruct.Closure c1 -> Stack.push (Lambda (c1,e)) s; (c, e, s, r)
+  | Instruct.Tailapply ->
+    let (c1,e1) = match Stack.pop s with
+        Lambda x -> x
+      | _ -> failwith "cannot tailapply with non closure" in
+    (c1, e1, s, r)
+  | Instruct.Apply ->
+    Stack.push (Lambda (c,e)) r;
+    let (c1,e1) = match Stack.pop s with
+        Lambda x -> x
+      | _ -> failwith "cannot apply non closure" in
+    (c1, e1, s, r)
+  | Instruct.Pushmark -> Stack.push Eplison s; (c, e, s, r)
+  | Instruct.Grab ->
+    if Stack.top s = Eplison then
+      (Stack.pop s |> ignore;
+       Stack.push (Lambda (Instruct.Grab::c,e)) s;
+       (let (c1,e1) = match Stack.pop r with
+             Lambda x -> x
+           | _ -> failwith "should be closure in r" in
+        (c1, e1, s, r)))
+    else let v = Stack.pop s in
+      Stack.push v r;
+      (c, e, s, r)
+  | Instruct.Return ->
+    let v = Stack.pop s in
+    (if Stack.top s = Eplison then
+      (Stack.pop s |> ignore; Stack.push v s;
+       let (c1,e1) = match Stack.pop r with
+           Lambda x -> x
+         | _ -> failwith "should be closure in return" in
+       (c1, e1, s, r))
+    else match v with
+        Lambda (c1,e1) -> (c1, e1, s, r)
+      | _ -> failwith "should be")
+  | _ -> failwith "not implement"
 
-let run code e s =
-let rec loop (c, e, s) =
+let run code e s r =
+let rec loop (c, e, s, r) =
   match c with
   | [] -> failwith "must stop with Instruct.Stop"
   | [Instruct.Stop] -> Stack.top s
-  | op::c1 -> loop (step (c1, e, s) op)
-in loop (code, e, s)
+  | op::c1 -> loop (step (c1, e, s, r) op)
+in loop (code, e, s, r)
 
 let input0 = Lambda.App ((Lambda.App ((Lambda.Fun (2,Lambda.Var 1)), [Lambda.Int 3])), [Lambda.Int 5]);;
 (* let input1 = compile input0 [Instruct.Stop];; *)
